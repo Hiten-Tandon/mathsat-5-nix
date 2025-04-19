9275a43a9a796028b77d593a8851ec0700670d8c
Hiten Tandon <tandonhiten@gamil.com>
2025-04-19 14:28:21 +0530

    Refactor: Made the flake more readable

--- flake.nix
+++ flake.nix
@@ -7,11 +7,11 @@
   outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "aarch64-darwin"]  (system: 
     with import nixpkgs {inherit system;};
     let
-        pname = "MathSAT";
+        pname = "mathsat";
         version = "5.6.11";
-        url = if stdenv.isLinux && stdenv.isx86_64 then "https://mathsat.fbk.eu/release/mathsat-5.6.11-linux-x86_64-reentrant.tar.gz" 
-              else if stdenv.isLinux && stdenv.isAarch64 then "https://mathsat.fbk.eu/release/mathsat-5.6.11-linux-aarch64-reentrant.tar.gz" 
-              else "https://mathsat.fbk.eu/release/mathsat-5.6.11-osx.tar.gz";
+        url = if stdenv.isLinux && stdenv.isx86_64 then "https://mathsat.fbk.eu/release/${pname}-${version}-linux-x86_64-reentrant.tar.gz" 
+              else if stdenv.isLinux && stdenv.isAarch64 then "https://mathsat.fbk.eu/release/${pname}-${version}-linux-aarch64-reentrant.tar.gz" 
+              else "https://mathsat.fbk.eu/release/${pname}-${version}-osx.tar.gz";
         sha256 = if stdenv.isLinux && stdenv.isx86_64 then "1f44klj240qa71cd00lxwv67ryrrx8saydm0zsz6zdnn6fnz0j1r" 
                  else if stdenv.isLinux && stdenv.isAarch64 then "1b7c1wg05870kz4f9r8sfmqsrhxv1qwmajb7gjvsy8bdzlh5h567"
                  else "1a8c8mkjjp0as470yj8yl9nzz040ja7grx40ykxip1hh2f8yn6n3";
